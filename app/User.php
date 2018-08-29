347ddb3549b9c8ec98ea06606a05a19f6a5be56c
BeiFangkx <774921903@qq.com>
2018-08-29 14:19:59 +0800

    第二次提交

--- app/User.php
+++ app/User.php
@@ -10,10 +10,11 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass assignable.//////
      *
      * @var array
      */
+    
     protected $fillable = [
         'name', 'email', 'password',
     ];
